e593f92ebb8fa5e675aac3b9931b5fe2542f4085
David Bradway <david.bradway@gmail.com>
2014-10-08 10:31:21 -0400

    fix type on startup script

--- startup.m
+++ startup.m
@@ -3,5 +3,4 @@ format compact %change matlab prefs
 
 cfu_tools_init_verbose=0; run /getlab/dpb6/repos/dtucode/CFUtools/init.m
 
-addpath('/getlab/dpb6/repos/code/');' pth=paths();
-
+addpath('/getlab/dpb6/repos/code/'); pth=paths();
