0a959da6baa1d68ed8c338c6149e0cb8a5de31c1
David Bradway <dpb6@duke.edu>
2016-09-26 15:12:07 -0400

    add better help and make current figure default

--- addGIFFrame.m
+++ addGIFFrame.m
@@ -1,15 +1,25 @@
 
 function addGIFFrame(filename,fighandle,delayTime)
-% %Make an animated GIF
-% filename = 'testnew.gif';
-% fighandle = figure(1);
-% delayTime = 1; % delay time between frames in Seconds
-% for i = 1:5
-%     plot(i,rand(1),'o'),hold on
-%     axis([0,6,0,1])
-%     addGIFFrame(filename,fighandle)
-%     pause(.5)
-% end
+% ADDGIFFRAME Append a frame to a GIF from a Figure.
+%   ADDGIFFRAME(FILENAME,FIGHANDLE,DELAYTIME) adds a frame to FILENAME from
+%                                             FIGHANDLE with DELAYTIME sec
+%   ADDGIFFRAME(FILENAME,FIGHANDLE) adds a frame to FILENAME from FIGHANDLE
+%                                   with 1 sec delay between frames
+%   ADDGIFFRAME(FILENAME) adds a frame to FILENAME from the current figure
+%                         with 1 sec delay between frames
+%   ADDGIFFRAME() adds a frame to 'file.gif' from the current figure with 1
+%                 sec delay between frames
+%
+% Make an animated GIF, Example:
+%filename = 'testnew.gif';
+%fighandle = figure(1);
+%delayTime = 1; % delay time between frames in Seconds
+%for i = 1:5
+%    plot(i,rand(1),'o'),hold on
+%    axis([0,6,0,1])
+%    addGIFFrame(filename,fighandle,delayTime)
+%    pause(.5)
+%end
 
 switch nargin
     case 3
@@ -18,10 +28,10 @@ switch nargin
         delayTime = 1;
     case 1
         delayTime = 1;
-        fighandle = 101;
+        fighandle = gcf;
     case 0
         delayTime = 1;
-        fighandle = 101;
+        fighandle = gcf;
         filename = 'file.gif';
     otherwise
         error('must have <= 3 arguments');
