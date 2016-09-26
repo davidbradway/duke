06a46ff2570cc7e9b4b1941494b0a9188a15198f
David Bradway <dpb6@duke.edu>
2016-09-26 12:45:25 -0400

    improve example code

--- addGIFFrame.m
+++ addGIFFrame.m
@@ -1,9 +1,14 @@
 
 function addGIFFrame(filename,fighandle)
 % %Make an animated GIF
-%filename = 'testnew51.gif';
-%fighandle=figure(proc.figure);
-%addGIFFrame(filename,fighandle)
+% filename = 'testnew.gif';
+% fighandle=figure(1);
+% for i = 1:5
+%     plot(i,rand(1),'o'),hold on
+%     axis([0,6,0,1])
+%     addGIFFrame(filename,fighandle)
+%     pause(.5)
+% end
 
 figure(fighandle);
 [imind,cm] = rgb2ind(frame2im(getframe(fighandle)),256);
@@ -14,4 +19,3 @@ if ~exist(filename,'file')
 else % file already exists, so append to it.
     imwrite(imind,cm,filename,'gif','WriteMode','append');
 end
-
